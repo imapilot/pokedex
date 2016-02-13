bc0fa70710cccc1a97b2cb2a159dbc3d1e70fcfa
Neville Smythe <nevillesmythe@nevilles-imac.local>
2016-02-13 22:20:06 +1000

    Initial Commit

---
+++ pokedex-by-me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-me
+//
+//  Created by Neville Smythe on 13/02/2016.
+//  Copyright © 2016 Neville Smythe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
