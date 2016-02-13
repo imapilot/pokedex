502bd46cf0b7a7fef78d78ae03487bf82b4d7be5
Neville Smythe <nevillesmythe@Nevilles-iMac.local>
2016-02-13 22:49:03 +1000

    Added pokemon class

---
+++ pokedex-by-me/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex-by-me
+//
+//  Created by Neville Smythe on 13/02/2016.
+//  Copyright © 2016 Neville Smythe. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+        
+}
