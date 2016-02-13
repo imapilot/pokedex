7209b8850739d0b9bfcb6142990f083e475993c6
Neville Smythe <nevillesmythe@Nevilles-iMac.local>
2016-02-13 23:11:34 +1000

    Added PokeCell UICollectionViewCell and linked to storyboard

---
+++ pokedex-by-me/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex-by-me
+//
+//  Created by Neville Smythe on 13/02/2016.
+//  Copyright © 2016 Neville Smythe. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+}
